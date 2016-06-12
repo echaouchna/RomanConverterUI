6031d97d1e1dfb12150d661d55ea4b03edefedd7
Nabil Echaouch <echaouchna@gmail.com>
2016-06-12 02:43:36 +0200

    Initial commit

---
+++ js/components.js
@@ -0,0 +1,57 @@
+(function(React, ReactDOM, Reflux, ReactBootstrap, converterActions, converterStore, global) {
+
+    var ConverterApp = React.createClass({
+        mixins: [Reflux.connect(converterStore, 'numbers')],
+        convertArabicToRoman: function(e){
+            var numbers = this.state.numbers;
+            numbers.arabic = e.target.value;
+            this.setState({numbers: numbers});
+            converterActions.convertArabicToRoman();
+        },
+        convertRomanToArabic: function(e){
+            var numbers = this.state.numbers;
+            numbers.roman = e.target.value.toUpperCase();
+            this.setState({numbers: numbers});
+            converterActions.convertRomanToArabic();
+        },
+        render: function() {
+            var n = this.state.numbers;
+            // return (
+            //     <div>
+            //         <input type="text" value={n.arabic} onChange={this.convertArabicToRoman}></input>
+            //         <input type="text" value={n.roman} onChange={this.convertRomanToArabic}></input>
+            //     </div>
+            // );
+            var InputGroup = ReactBootstrap.InputGroup;
+            var FormControl = ReactBootstrap.FormControl;
+            var Alert = ReactBootstrap.Alert;
+            if (n.error) {
+                return (
+                    <div>
+                        <InputGroup>
+                            <FormControl type="text" placeholder="Arabic number" value={n.arabic} onChange={this.convertArabicToRoman}></FormControl>
+                            <InputGroup.Addon>-</InputGroup.Addon>
+                            <FormControl type="text" placeholder="Roman number" value={n.roman} onChange={this.convertRomanToArabic}></FormControl>
+                        </InputGroup>
+                        <br/>
+                        <Alert bsStyle="danger">{n.error}</Alert>
+                    </div>
+                );
+            } else {
+                return (
+                    <div>
+                        <InputGroup>
+                            <FormControl type="text" placeholder="Arabic number" value={n.arabic} onChange={this.convertArabicToRoman}></FormControl>
+                            <InputGroup.Addon>-</InputGroup.Addon>
+                            <FormControl type="text" placeholder="Roman number" value={n.roman} onChange={this.convertRomanToArabic}></FormControl>
+                        </InputGroup>
+                    </div>
+                );
+            }
+        }
+    });
+
+    ReactDOM.render(
+        <ConverterApp/>, document.getElementById('content'));
+
+})(window.React, window.ReactDOM, window.Reflux, window.ReactBootstrap, window.converterActions, window.converterStore, window);
---
+++ js/store.js
@@ -0,0 +1,78 @@
+(function(Reflux, converterActions, global) {
+    'use strict';
+
+    var person = {
+        name: "Nabil",
+        age: 29
+    };
+
+    var numbers = {
+        roman: "",
+        arabic: "",
+        error: ""
+    };
+
+    global.converterStore = Reflux.createStore({
+        listenables: [converterActions],
+        onConvertRomanToArabic: function() {
+            console.log('roman = ' + numbers.roman);
+            numbers.error = "";
+            if (numbers.roman === "") {
+                numbers.arabic = "";
+                this.trigger(numbers);
+            } else {
+                self = this;
+                $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                    crossDomain: true,
+                    url: "http://localhost:9090/api/convertToArabic?roman=" + numbers.roman,
+                    success: function(data) {
+                        console.log("success");
+                        console.log(JSON.stringify(data));
+                        numbers.arabic = data;
+                        self.trigger(numbers);
+                    },
+                    error: function(data) {
+                        console.log("error");
+                        console.log(JSON.stringify(data));
+                        numbers.error = "Wrong roman number";
+                        self.trigger(numbers);
+                    }
+                });
+            }
+        },
+        onConvertArabicToRoman: function() {
+            console.log('arabic = ' + numbers.arabic);
+            numbers.error = "";
+            if (numbers.arabic === "") {
+                numbers.roman = "";
+                this.trigger(numbers);
+            } else {
+                self = this;
+                $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                    crossDomain: true,
+                    url: "http://localhost:9090/api/convertToRoman?arabic=" + numbers.arabic,
+                    success: function(data) {
+                        console.log("success");
+                        console.log(JSON.stringify(data));
+                        numbers.roman = data.message;
+                        self.trigger(numbers);
+                    },
+                    error: function(data) {
+                        console.log("error");
+                        console.log(JSON.stringify(data));
+                        numbers.error = "Wrong arabic number, You shoud use integer in the range [1, 3999]";
+                        self.trigger(numbers);
+                    }
+                });
+            }
+        },
+        getInitialState: function() {
+            return numbers;
+        }
+    });
+
+})(window.Reflux, window.converterActions, window);
